5b9ef7f4211df6aad668de17f9b476ef7a1f733f
AksiDimon <aksbum@gmail.com>
2025-07-29 15:45:46 +0300

    setted gh-pages

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // указываем, под каким путём будет развернуто приложение на GitHub Pages
+  base: '/front-end-entrance-exam/'
+})
